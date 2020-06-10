5946e071c33137aba8fa54687fe5838c71c4db28
Sajjadkhan12 <sajjadkhan12@users.noreply.github.com>
2020-06-10 20:49:24 +0500

    added agents

--- zameen/urls.py
+++ zameen/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('shelter.urls')),
+    path('agents/',include('agents.urls')),
     # path('account/',include('users.urls')),
 ]
 if settings.DEBUG:
