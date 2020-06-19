0bf27efca944b5af3e47c8f543a4025549e8013c
Sajjadkhan12 <sajjadkhan12@users.noreply.github.com>
2020-06-19 13:46:53 +0500

    whitenoise problems

--- zameen/settings.py
+++ zameen/settings.py
@@ -127,4 +127,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- zameen/wsgi.py
+++ zameen/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zameen.settings')
 
 application = get_wsgi_application()
+
